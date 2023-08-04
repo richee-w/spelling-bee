7d5223f735dfbba1e351570fe0793c7856216c76
riche-w <contact@richeewilson.com>
2023-08-04 14:55:26 +0100

    Simple test, no real update

--- gui-spell-test.py
+++ gui-spell-test.py
@@ -3,9 +3,9 @@
 #
 # GNU General Public License, version 3
 
-###
+####
 # Spelling Contest: A program that generates a list of words based on the number of letters specified by the user.
-###
+####
 
 import tkinter as tk
 from tkinter import messagebox
